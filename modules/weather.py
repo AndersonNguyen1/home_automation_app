521524a7b2ed83989090b2419485ee2e94d84563
Anderson Nguyen <andersonvannguyen@gmail.com>
2020-06-23 09:37:35 -0700

    connected to weather API and stored data and city data

--- modules/weather.py
+++ modules/weather.py
@@ -1,1 +1,6 @@
-# everything related to the weather
+from keys import weather_key, state, city
+import requests
+print(f'http://api.openweathermap.org/data/2.5/forecast?q={city},{state}&appid={weather_key}')
+response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={city},{state}&appid={weather_key}')
+
+print(response)
