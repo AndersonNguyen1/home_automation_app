bec0c6096b24380cd23a4220ea6c047822b38c7e
Anderson Nguyen <andersonvannguyen@gmail.com>
2020-06-23 11:38:27 -0700

    put a main function inside app.py to run modules. Added __init__.py to modules directory

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 # Entry point for the app
 # Runs the code
+
+import sys
+from modules.weather import *
+
+get_weather_details()
--- modules/weather.py
+++ modules/weather.py
@@ -1,6 +1,17 @@
-from keys import weather_key, state, city
+from modules.keys import weather_key, state, city
 import requests
-print(f'http://api.openweathermap.org/data/2.5/forecast?q={city},{state}&appid={weather_key}')
-response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={city},{state}&appid={weather_key}')
 
-print(response)
+def get_weather_details():
+    temp, weather, wind_speed = 'null', 'null', 'null'
+
+    response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={city},{state}&appid={weather_key}')
+
+    if response.ok:
+        temp = response.json()["list"][0]["main"]["temp"]
+        weather = response.json()["list"][0]["weather"][0]["main"]
+        wind_speed = response.json()["list"][0]["wind"]["speed"]
+
+    print(temp)
+    print(weather)
+    print(wind_speed)
+
