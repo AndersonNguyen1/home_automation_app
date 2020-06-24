357f816828c167cea62af8f4a731675db58c4cb9
Anderson Nguyen <andersonvannguyen@gmail.com>
2020-06-24 11:13:22 -0700

    added Weather class and dunder main to weather.py

--- modules/weather.py
+++ modules/weather.py
@@ -1,19 +1,37 @@
-from modules.keys import weather_key, state, city
+from keys import city, state, weather_key
+from datetime import date, datetime, time, timedelta
 import requests
 
-def get_weather_details():
-    temp, weather, wind_speed = 'null', 'null', 'null'
+# def get_weather_details():
+#     temp, weather, wind_speed = 'null', 'null', 'null'
 
-    response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={city},{state}&units=imperial&appid={weather_key}')
+#     if response.ok:
+#         weather_data_total = len(response.json()["list"])
+#         dt = response.json()["list"][weather_data_total - 1]["dt"]
+#         temp = response.json()["list"][weather_data_total - 1]["main"]["temp"]
+#         weather = response.json()["list"][weather_data_total - 1]["weather"][0]["main"]
+#         wind_speed = response.json()["list"][weather_data_total - 1]["wind"]["speed"]
+    
+#     print(datetime.fromtimestamp(dt).strftime("%A, %B %d, %Y %I:%M:%S"))
+#     print(weather_data_total)
+#     print(temp)
+#     print(weather)
+#     print(wind_speed)
 
-    if response.ok:
-        weather_data_total = len(response.json()["list"])
+class Weather():
+    def __init__(self):
+        self.today = {}
+        self.tomorrow = {}
+        # p2 = plus 2
+        self.today_p2 = {}
+        self.today_p3 = {}
+        self.date = date.today()
+        self.time = time(15, 0, 0, 0)
 
-        temp = response.json()["list"][weather_data_total - 1]["main"]["temp"]
-        weather = response.json()["list"][weather_data_total - 1]["weather"][0]["main"]
-        wind_speed = response.json()["list"][weather_data_total - 1]["wind"]["speed"]
+    def get_data(self, key, city, state):
+        response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={city},{state}&units=imperial&appid={key}')
+        print(response.json()["list"])
 
-    print(temp)
-    print(weather)
-    print(wind_speed)
-
+if __name__ == "__main__":
+    weather = Weather()
+    weather.get_data(weather_key, city, state)
