1cd38982cdb80cdf9ed859ba6893947534ff7c5a
Anderson Nguyen <andersonvannguyen@gmail.com>
2020-06-24 11:48:20 -0700

    added logic to organize_data()

--- modules/weather.py
+++ modules/weather.py
@@ -1,22 +1,6 @@
-from keys import city, state, weather_key
+from keys import city, state, weather_key # add back module.keys if you want to run in app.py main()
 from datetime import date, datetime, time, timedelta
 import requests
-
-# def get_weather_details():
-#     temp, weather, wind_speed = 'null', 'null', 'null'
-
-#     if response.ok:
-#         weather_data_total = len(response.json()["list"])
-#         dt = response.json()["list"][weather_data_total - 1]["dt"]
-#         temp = response.json()["list"][weather_data_total - 1]["main"]["temp"]
-#         weather = response.json()["list"][weather_data_total - 1]["weather"][0]["main"]
-#         wind_speed = response.json()["list"][weather_data_total - 1]["wind"]["speed"]
-    
-#     print(datetime.fromtimestamp(dt).strftime("%A, %B %d, %Y %I:%M:%S"))
-#     print(weather_data_total)
-#     print(temp)
-#     print(weather)
-#     print(wind_speed)
 
 class Weather():
     def __init__(self):
@@ -30,7 +14,22 @@ class Weather():
 
     def get_data(self, key, city, state):
         response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={city},{state}&units=imperial&appid={key}')
-        print(response.json()["list"])
+        return self.organize_data(response, response.json()["list"])
+    
+    def organize_data(self, response, data):
+        temp, weather, wind_speed = 'null', 'null', 'null'
+
+        ## weather_data_total - 1 is the 40th day in 5day/3hr 
+        if response.ok:
+            weather_data_total = len(response.json()["list"])
+            dt = response.json()["list"][0]["dt"]
+            temp = response.json()["list"][weather_data_total - 1]["main"]["temp"]
+            weather = response.json()["list"][weather_data_total - 1]["weather"][0]["main"]
+            wind_speed = response.json()["list"][weather_data_total - 1]["wind"]["speed"]
+        
+        for data_point in data:
+            convert = datetime.fromtimestamp(dt).strftime("%A, %B %d, %Y %I:%M:%S")
+            print(convert)
 
 if __name__ == "__main__":
     weather = Weather()
